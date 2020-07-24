be00f2e126bd4855ca5b5388693799fe1052f168
Anuhya Ghorakavi <anuhya.ghorakavi@gmail.com>
2020-07-24 13:32:21 +0530

    feat(login): move button inside form and change type + make functional component

--- src/views/Auth/Login/index.js
+++ src/views/Auth/Login/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { PropTypes } from "prop-types";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Button,
   Form,
@@ -11,171 +10,154 @@ import {
   Image,
   Navbar,
 } from "react-bootstrap";
+import { getIsSubmitted } from '../../../redux/selectors/auth/login';
 import { loginActions } from "../../../redux/actions/authActions/loginActions";
 import "./login.scss";
 import "../index.scss";
 import mask from "./Mask Group.png";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    // initalizing the state to logout on landing
-    // loginActions.logout();
+  const dispatch = useDispatch();
+  const loginRequested = () => dispatch(loginActions.loginRequested);
+  const login = () => dispatch(loginActions.login);
+  const logout = () => dispatch(loginActions.logout);
+  const isSubmitted = useSelector(getIsSubmitted);
 
-    this.state = {
-      email: "",
-      password: "",
-    };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e, field) {
-    console.log("submit pressed");
+  const handleEmailInput = (e) => {
     const { value } = e.target;
-    this.setState({ [field]: value });
-  }
 
-  handleSubmit(e) {
-    console.log("submit pressed");
-    e.preventDefault();
-    this.props.loginRequested();
-    if (this.state.email && this.state.password) {
-      this.props.login(this.state.email, this.state.password);
+    if (value.length > 0) {
+      setEmail(value);
     }
   }
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          {/* Column for background image*/}
-          <Col>
-            <div className="sign-up-image">
-              <Card>
-                <Image src={mask} />
-                <Card.ImgOverlay>
-                  <Card.Text>Tapestry Pooling</Card.Text>
-                  <Card.Title>
-                    Towards faster <br /> testing
-                  </Card.Title>
-                  <Card.Subtitle>
-                    Tapestry, a novel quantitative nonadaptive pooling <br />
-                    scheme to test many samples using only a few tests.
-                  </Card.Subtitle>
-                </Card.ImgOverlay>
-              </Card>
-            </div>
-          </Col>
+  const handlePasswordInput = (e) => {
+    const { value } = e.target;
 
-          {/* Column for the Sign-up Box */}
-          <Col>
-            <Navbar>
-              <ul className="navbar-nav ml-auto">
-                <li>
-                  <a className="nav-link"> Develop using our Algorithm</a>
-                </li>
-                <li>
-                  <a className="nav-link" href="#">
-                    Contact us
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-link" href="#">
-                    Visit Website
-                  </a>
-                </li>
-              </ul>
-            </Navbar>
+    if (value.length > 0) {
+      setPassword(value);
+    }
+  }
 
-            <div className="login">
-              <Card border="light">
-                <Card.Title>WELCOME</Card.Title>
+  const handleSubmit = e => {
+    console.log("submit pressed");
+    e.preventDefault();
+    loginRequested();
+    if (email && password) {
+      login(email, password);
+    }
+  };
+
+  return (
+    <Container fluid>
+      <Row>
+        {/* Column for background image*/}
+        <Col>
+          <div className="sign-up-image">
+            <Card>
+              <Image src={mask} />
+              <Card.ImgOverlay>
+                <Card.Text>Tapestry Pooling</Card.Text>
+                <Card.Title>
+                  Towards faster <br /> testing
+                </Card.Title>
                 <Card.Subtitle>
-                  Login using your registered Email and password.
-                </Card.Subtitle>{" "}
-                <br />
-                <Form onSubmit={(e) => this.handleSubmit(e)}>
-                  {/* //{this.handleSubmit}> */}
-                  <Form.Group
-                    as={Row}
-                    controlId="formHorizontalEmail"
-                    role="form"
-                  >
-                    <Col sm={8}>
-                      <Form.Control
-                        className="input"
-                        type="email"
-                        placeholder="Email Address"
-                        size="lg"
-                        value={this.state.email}
-                        onChange={(e) => this.handleChange(e, "email")}
-                      />
-                    </Col>
-                  </Form.Group>
-                  {this.props.isSubmitted && !this.state.email && (
-                    <Form.Text className="alert">Email is required.</Form.Text>
-                  )}
-                  <Form.Group
-                    as={Row}
-                    controlId="formBasicPassword"
-                    role="form"
-                  >
-                    <Col sm={8}>
-                      <Form.Control
-                        classname="input"
-                        type="password"
-                        placeholder="Password"
-                        size="lg"
-                        value={this.state.password}
-                        onChange={(e) => this.handleChange(e, "password")}
-                      />
-                    </Col>
-                  </Form.Group>
-                  {this.props.isSubmitted && !this.state.password && (
-                    <Form.Text className="alert">
-                      Password is required.
-                    </Form.Text>
-                  )}
-                </Form>
+                  Tapestry, a novel quantitative nonadaptive pooling <br />
+                  scheme to test many samples using only a few tests.
+                </Card.Subtitle>
+              </Card.ImgOverlay>
+            </Card>
+          </div>
+        </Col>
+
+        {/* Column for the Sign-up Box */}
+        <Col>
+          <Navbar>
+            <ul className="navbar-nav ml-auto">
+              <li>
+                <a className="nav-link"> Develop using our Algorithm</a>
+              </li>
+              <li>
+                <a className="nav-link" href="#">
+                  Contact us
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" href="#">
+                  Visit Website
+                </a>
+              </li>
+            </ul>
+          </Navbar>
+
+          <div className="login">
+            <Card border="light">
+              <Card.Title>WELCOME</Card.Title>
+              <Card.Subtitle>
+                Login using your registered Email and password.
+              </Card.Subtitle>{" "}
+              <br />
+              <Form onSubmit={handleSubmit}>
+                <Form.Group
+                  as={Row}
+                  controlId="formHorizontalEmail"
+                  role="form"
+                >
+                  <Col sm={8}>
+                    <Form.Control
+                      className="input"
+                      type="email"
+                      placeholder="Email Address"
+                      size="lg"
+                      value={email}
+                      onChange={handleEmailInput}
+                    />
+                  </Col>
+                </Form.Group>
+                {isSubmitted && !email && (
+                  <Form.Text className="alert">Email is required.</Form.Text>
+                )}
+                <Form.Group
+                  as={Row}
+                  controlId="formBasicPassword"
+                  role="form"
+                >
+                  <Col sm={8}>
+                    <Form.Control
+                      classname="input"
+                      type="password"
+                      placeholder="Password"
+                      size="lg"
+                      value={password}
+                      onChange={handlePasswordInput}
+                    />
+                  </Col>
+                </Form.Group>
+                {isSubmitted && !password && (
+                  <Form.Text className="alert">
+                    Password is required.
+                  </Form.Text>
+                )}
                 <Card.Text>
                   <a href="/reset-password">Forgot Password?</a>
                 </Card.Text>
-                <Button variant="primary" size="lg" value="submit">
+                <Button variant="primary" size="lg" type="submit">
                   Login
                 </Button>
-                <Card.Text className="new-user">
-                  New user? <a href="/sign-up">Contact us</a> to register.
-                </Card.Text>
-              </Card>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+              </Form>
+              <Card.Text className="new-user">
+                New user? <a href="/sign-up">Contact us</a> to register.
+              </Card.Text>
+            </Card>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-Login.propTypes = {
-  isSubmitted: PropTypes.bool.isRequired,
-};
-
-function mapStateToProps(state) {
-  return {
-    isSubmitted: state.isSubmitted,
-  };
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  loginRequested: () => dispatch(loginActions.loginRequested),
-  login: () => dispatch(loginActions.login),
-  logout: () => dispatch(loginActions.logout),
-
-  // logout: loginActions.logout,
-  // login: loginActions.login,
-  // loginRequested: loginActions.loginRequested,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
