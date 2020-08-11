b5ec58c01117cebf900e1240138b3bcdcfedbe2f
prantik1998 <prantikchakraborty50@gmail.com>
2020-08-11 23:41:33 +0530

    before the pull

---
+++ src/components/Dropzone/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './Dropzone.css'
+
+class Dropzone extends Component {
+    constructor(props) {
+        super(props);
+        this.fileInputRef = React.createRef();
+        this.openFileDialog = this.openFileDialog.bind(this);
+        }
+
+    openFileDialog() {
+        if (this.props.disabled) return;
+        this.fileInputRef.current.click();
+        }
+
+  render() {
+    return (
+      <div className="Dropzone" onClick={this.openFileDialog} style={{ cursor: this.props.disabled ? "default" : "pointer"}}>
+        <img
+          alt="upload"
+          className="Icon"
+          src="baseline-cloud_upload-24px.svg"
+        />
+         <input
+          ref={this.fileInputRef}
+          className="FileInput"
+          type="file"
+          multiple
+          onChange={this.onFilesAdded}
+        />
+        <span>Upload Files</span>
+      </div>
+    )
+  }
+}
