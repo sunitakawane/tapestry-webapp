791aec5a69ac5805d62dd3f223d922938384c5ff
Akanksha Vyas <vyasakanksha@gmail.com>
2020-08-17 12:24:01 +0530

    fixing the base API url

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,6 +1,6 @@
 const url = {
-  // BASE_API_URL: "https://tapestry-pooling-284109.ew.r.appspot.com/",
-  BASE_API_URL: "http://127.0.0.1:8000/"
+  BASE_API_URL: "https://tapestry-pooling-284109.ew.r.appspot.com/",
+  // BASE_API_URL: "http://127.0.0.1:8000/"
 };
 
 export default url;
